4485d562d81c728f7120e768e5324a65f4c26f9c
Adrilene <adrilenefonseca@gmail.com>
2022-10-31 21:46:36 -0300

    bubble sort algorithm

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(elements_list):
+    for i in range(len(elements_list)):
+        for j in range(i + 1, len(elements_list)):
+            if elements_list[j] < elements_list[i]:
+                aux = elements_list[i]
+                elements_list[i] = elements_list[j]
+                elements_list[j] = aux
+
+    return elements_list
---
+++ main.py
@@ -0,0 +1,10 @@
+from bubble_sort import bubble_sort
+
+
+def main():
+    elements_list = [5, 4, 6, 1, 60, 7, 2, 12]
+    print(bubble_sort(elements_list))
+
+
+if __name__ == "__main__":
+    main()
