2b3548c415cc6cf499858ea4245287edca5caf43
Adrilene <adrilenefonseca@gmail.com>
2022-11-01 10:00:40 -0300

    adding readme and main file

---
+++ README.md
@@ -0,0 +1,1 @@
+#Studying the main sort algorithms
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from bubble_sort import bubble_sort
+from selection_sort import selection_sort
+from insertion_sort import insertion_sort
+from quick_sort import quick_sort
 
 
 def main():
     elements_list = [5, 4, 6, 1, 60, 7, 2, 12]
-    print(bubble_sort(elements_list))
+    print(quick_sort(elements_list, 0, 7))
 
 
 if __name__ == "__main__":
